49966cce9d1d186ebd73c61ca07f5e70367b1a8f
AltLilac <49546834+AltLilac@users.noreply.github.com>
2021-03-18 13:05:57 +0900

    Attach と Interact のインターフェースを用意した
    BP 上でオーバーライドできるようにした。

---
+++ ShootingRange/Source/ShootingRange/Player/Interface/CPP_Attach.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Attach.h"
+
+// Add default functionality here for any ICPP_Attach functions that are not pure virtual.
+void ICPP_Attach::AttachItemToHand_Implementetion()
+{
+
+}
---
+++ ShootingRange/Source/ShootingRange/Player/Interface/CPP_Interact.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Interact.h"
+
+// Add default functionality here for any ICPP_Interact functions that are not pure virtual.
+void ICPP_Interact::Interact_Implementation()
+{
+
+}
